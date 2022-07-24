99df91bbb9072cfb479b455e5010d9e640771786
chlwlgns524 <chlwlgns524@naver.com>
2022-07-24 14:13:08 +0900

    프로젝트 초기화

---
+++ src/main/java/com/tasty/masiottae/TeamTastyMasiottaeBeApplication.java
@@ -0,0 +1,13 @@
+package com.tasty.masiottae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamTastyMasiottaeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamTastyMasiottaeBeApplication.class, args);
+    }
+
+}
