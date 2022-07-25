ed19bda67e5bf52398dbdfdbf79410366a60ce4f
wisehero10 <godjw96@gmail.com>
2022-07-25 20:56:55 +0900

    chore : checkStyle Indent 4로 변경

--- src/main/java/com/tasty/masiottae/TeamTastyMasiottaeBeApplication.java
+++ src/main/java/com/tasty/masiottae/TeamTastyMasiottaeBeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TeamTastyMasiottaeBeApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(TeamTastyMasiottaeBeApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(TeamTastyMasiottaeBeApplication.class, args);
+    }
 
 }
