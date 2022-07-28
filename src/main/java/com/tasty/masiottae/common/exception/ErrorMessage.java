b674365a25e37210dd294131675b590cc759096b
wisehero10 <godjw96@gmail.com>
2022-07-28 17:18:48 +0900

    fix : conflict 해결

--- src/main/java/com/tasty/masiottae/common/exception/ErrorMessage.java
+++ src/main/java/com/tasty/masiottae/common/exception/ErrorMessage.java
@@ -8,8 +8,6 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorMessage {
 
     INVALID_INPUT_VALUE("잘못된 데이터를 입력하였습니다."),
-    NOT_FOUND_MENU_ERR("존재하지 않는 메뉴입니다."),
-    // s3
     IMAGE_SAVE_ERROR("이미지 업로드에 문제가 발생하였습니다.");
 
     private final String message;
