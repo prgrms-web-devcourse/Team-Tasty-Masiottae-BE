6e59294ea286fba77fdb53272d22a07265511750
wisehero10 <godjw96@gmail.com>
2022-07-28 17:13:24 +0900

    fix : conflict 수정

--- src/main/java/com/tasty/masiottae/menu/domain/Taste.java
+++ src/main/java/com/tasty/masiottae/menu/domain/Taste.java
@@ -1,15 +1,12 @@
 package com.tasty.masiottae.menu.domain;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -22,11 +19,16 @@ public class Taste {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "taste_name")
     private String tasteName;
+    private String tasteColor;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "menu_id")
-    private Menu menu;
+    @Builder
+    private Taste(String tasteName, String tasteColor) {
+        this.tasteName = tasteName;
+        this.tasteColor = tasteColor;
+    }
+
+    public static Taste createTaste(String tasteName, String tasteColor) {
+        return new Taste(tasteName, tasteColor);
+    }
 }
