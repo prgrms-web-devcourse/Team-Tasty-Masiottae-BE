3c2f98f67e7187c22ebbc7740819fe8e2c824f68
choi hyun woong <choihyeon0517@naver.com>
2022-07-28 18:41:16 +0900

    feat: SecurityConfig에 필터 적용

--- src/main/java/com/tasty/masiottae/security/config/SecurityConfig.java
+++ src/main/java/com/tasty/masiottae/security/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.tasty.masiottae.security.config;
 
 
+import com.tasty.masiottae.security.filter.JwtAuthenticationFilter;
+import com.tasty.masiottae.security.filter.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,20 +10,28 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+
     @Bean
-    public BCryptPasswordEncoder passwordEncoderFactories() {
+    public PasswordEncoder passwordEncoderFactories() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception {
+        jwtAuthenticationFilter.setFilterProcessesUrl("/login");
 
         http.
                 csrf().disable()
@@ -29,7 +39,10 @@ public class SecurityConfig {
                 .httpBasic().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authorizeRequests().antMatchers("*").permitAll();
+                .authorizeRequests().antMatchers("*").permitAll()
+                .and()
+                .addFilter(jwtAuthenticationFilter)
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
