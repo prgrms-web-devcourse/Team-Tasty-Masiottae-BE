ec9f788772de56e7ea2381ffd41100aaf3bdaa32
choi hyun woong <choihyeon0517@naver.com>
2022-08-01 17:59:59 +0900

    feat : 서버 로그 저장

--- deploy.sh
+++ deploy.sh
@@ -19,4 +19,4 @@ else
 fi
 
 echo "> $JAR_PATH deploy"
-nohup java -jar -Dspring.profiles.active=prod $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar -Dspring.profiles.active=prod $JAR_PATH > /dev/null 2> /dev/null < /dev/null & >> result.log
