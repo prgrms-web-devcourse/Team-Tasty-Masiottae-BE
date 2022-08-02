9d7511ea13cef7741121f9fcf61ce211f872c059
chlwlgns524 <chlwlgns524@naver.com>
2022-08-02 11:42:34 +0900

    fix: 계정 엔티티 필드명 변경에 따른 메소드명 수정

--- src/main/java/com/tasty/masiottae/account/repository/AccountRepository.java
+++ src/main/java/com/tasty/masiottae/account/repository/AccountRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
-    Boolean existsByNickname(String nickname);
+    Boolean existsByNickName(String nickname);
 
     Boolean existsByEmail(String email);
 
