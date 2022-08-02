09676f2ee55e265776f6dc9a6686268b49214445
haemin-jeong <jhm0828@gmail.com>
2022-08-02 15:19:33 +0900

    feat: Taste 생성, 전체 조회 API 개발 및 테스트

--- src/main/java/com/tasty/masiottae/menu/TasteConverter.java
+++ src/main/java/com/tasty/masiottae/menu/TasteConverter.java
@@ -2,13 +2,18 @@ package com.tasty.masiottae.menu;
 
 import com.tasty.masiottae.menu.domain.Taste;
 import com.tasty.masiottae.menu.dto.TasteFindResponse;
+import com.tasty.masiottae.menu.dto.TasteSaveRequest;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TasteConverter {
 
     public TasteFindResponse toTasteFindResponse(Taste taste) {
-        return new TasteFindResponse(taste.getId(), taste.getTasteName());
+        return new TasteFindResponse(taste.getId(), taste.getTasteName(), taste.getTasteColor());
+    }
+
+    public Taste toTaste(TasteSaveRequest request) {
+        return Taste.createTaste(request.tasteName(), request.tasteColor());
     }
 
 }
