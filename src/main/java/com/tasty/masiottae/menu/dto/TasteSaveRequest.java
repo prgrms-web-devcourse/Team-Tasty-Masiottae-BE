e57e345c2ce2ba3580a1ef0ef4d957c01e8207f9
haemin-jeong <jhm0828@gmail.com>
2022-08-02 16:07:42 +0900

    fix: 색상 코드 정규표현식 수정

--- src/main/java/com/tasty/masiottae/menu/dto/TasteSaveRequest.java
+++ src/main/java/com/tasty/masiottae/menu/dto/TasteSaveRequest.java
@@ -7,6 +7,6 @@ public record TasteSaveRequest(
         @NotBlank(message = "공백 또는 널 값을 허용하지 않습니다.")
         String tasteName,
         @NotBlank(message = "공백 또는 널 값을 허용하지 않습니다.")
-        @Pattern(regexp = "/^#(?:[0-9a-f]{3}){1,2}$/i", message = "올바른 색상 코드 형식이 아닙니다.")
+        @Pattern(regexp = "(#?([a-fA-F0-9]{3})([a-fA-F0-9]{3}?))", message = "올바른 색상 코드 형식이 아닙니다.")
         String tasteColor) {
 }
