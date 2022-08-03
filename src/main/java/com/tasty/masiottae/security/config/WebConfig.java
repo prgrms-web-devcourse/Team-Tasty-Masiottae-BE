062aa1241b4fe434b85de026d47897b190194d7b
wisehero10 <godjw96@gmail.com>
2022-08-03 19:54:24 +0900

    chore : webconfig 대신 corsconfig에서 설정

--- src/main/java/com/tasty/masiottae/security/config/WebConfig.java
+++ src/main/java/com/tasty/masiottae/security/config/WebConfig.java
@@ -10,8 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedMethods("*")
-            .allowedOriginPatterns("*")
             .exposedHeaders("Location");
     }
 }
