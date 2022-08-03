359ce2f2668f44b97c515e4a948c5501c558164f
wisehero10 <godjw96@gmail.com>
2022-08-03 19:11:19 +0900

    fix : cors 매핑 설정 변경

--- src/main/java/com/tasty/masiottae/security/config/WebConfig.java
+++ src/main/java/com/tasty/masiottae/security/config/WebConfig.java
@@ -10,6 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .exposedHeaders("Location");
+            .allowedMethods("*")
+            .allowedOriginPatterns("*")
+            .exposedHeaders("Location");
     }
 }
