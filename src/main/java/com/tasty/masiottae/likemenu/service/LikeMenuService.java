f8abdc309d4c22bea9284572528a8f28a9fa361e
choi hyun woong <choihyeon0517@naver.com>
2022-08-05 18:20:28 +0900

    feat: 좋아요 추가, 제거 및 account로 검색 추가

---
+++ src/main/java/com/tasty/masiottae/likemenu/service/LikeMenuService.java
@@ -0,0 +1,63 @@
+package com.tasty.masiottae.likemenu.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.tasty.masiottae.account.domain.Account;
+import com.tasty.masiottae.common.exception.custom.NotFoundException;
+import com.tasty.masiottae.likemenu.domain.LikeMenu;
+import com.tasty.masiottae.likemenu.domain.LikeMenuId;
+import com.tasty.masiottae.likemenu.repository.LikeMenuRepository;
+import com.tasty.masiottae.menu.MenuConverter;
+import com.tasty.masiottae.menu.domain.Menu;
+import com.tasty.masiottae.menu.dto.MenuFindResponse;
+import com.tasty.masiottae.menu.repository.MenuRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class LikeMenuService {
+
+    private final LikeMenuRepository likeMenuRepository;
+    private final MenuRepository menuRepository;
+    private final MenuConverter menuConverter;
+
+    private void addLike(Account account, Menu menu) {
+        likeMenuRepository.save(new LikeMenu(account, menu));
+        menu.addLike();
+    }
+
+    private void cancelLike(Account account, Menu menu) {
+        LikeMenuId likeMenuId = new LikeMenuId(account.getId(), menu.getId());
+        LikeMenu likeMenu = likeMenuRepository.findById(likeMenuId).orElseThrow();
+        likeMenu.getMenu().removeLike();
+        likeMenuRepository.delete(likeMenu);
+    }
+
+    @Transactional
+    public void changeLike(Account account, Long menuId) {
+        Menu menu = menuRepository.findByIdForUpdate(menuId).orElseThrow();
+        boolean isLike = likeMenuRepository.existByAccountAndMenu(account, menu);
+        if (isLike) {
+            cancelLike(account, menu);
+        } else {
+            addLike(account, menu);
+        }
+    }
+
+    public List<MenuFindResponse> getLikeMenu(Account account) {
+        List<LikeMenu> likeMenuList = likeMenuRepository.findEntityGraphNByAccount(
+                account);
+
+        return likeMenuList.stream()
+                .map(LikeMenu::getMenu)
+                .map(menuConverter::toMenuFindResponse)
+                .toList();
+    }
+
+
+}
