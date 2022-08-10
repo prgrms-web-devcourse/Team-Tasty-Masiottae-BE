d07278eafe64ed46728ca38f67687773e3515967
wisehero10 <godjw96@gmail.com>
2022-08-10 21:07:18 +0900

    refactor : 불필요한 주석 및 개행 제거

--- src/main/java/com/tasty/masiottae/comment/repository/CommentRepository.java
+++ src/main/java/com/tasty/masiottae/comment/repository/CommentRepository.java
@@ -9,11 +9,10 @@ import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-
     @Query("select c from Comment c left join fetch c.account a "
         + "left join fetch c.menu where c.id = :commentId")
     Optional<Comment> findByIdFetch(@Param("commentId") Long commentId);
 
     @Query("select c from Comment c where c.menu.id = :menuId")
-    List<Comment> findAllByMenuId(@Param("menuId")Long menuId);
+    List<Comment> findAllByMenuId(@Param("menuId") Long menuId);
 }
