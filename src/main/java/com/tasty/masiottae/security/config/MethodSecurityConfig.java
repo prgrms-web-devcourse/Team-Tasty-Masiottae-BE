8d22276c2bd7e116f4cde7591fe86ad895f86495
choi hyun woong <choihyeon0517@naver.com>
2022-08-12 17:13:33 +0900

    feat: method security

---
+++ src/main/java/com/tasty/masiottae/security/config/MethodSecurityConfig.java
@@ -0,0 +1,12 @@
+package com.tasty.masiottae.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+@Configuration
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true
+)
+public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+}
